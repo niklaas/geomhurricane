a3c14d0905ef8c9da47094d01b5a67a2359c0e64
Niklaas Baudet von Gersdorff <stdin@niklaas.eu>
2017-10-08 22:37:00 +0200

    (Re-)Exports pipe operators correctly

---
+++ R/pipe.R
@@ -0,0 +1,15 @@
+#' Pipe
+#'
+#' @importFrom magrittr %>%
+#' @name %>%
+#' @rdname pipe
+#' @export
+NULL
+
+#' Pipe and store
+#'
+#' @importFrom magrittr %<>%
+#' @name %<>%
+#' @rdname pipe-store
+#' @export
+NULL
